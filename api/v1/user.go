1b5c1bf8e39bc7fb51de23bb74916c328c086426
summerice <1146494446@qq.com>
2022-10-28 11:53:16 +0800

    添加用户接口

--- api/v1/user.go
+++ api/v1/user.go
@@ -1,17 +1,38 @@
 package v1
 
 import (
+	"net/http"
+	"blog/model"
+	"blog/utils/errmsg"
+
 	"github.com/gin-gonic/gin"
 )
 
-// 查询用户名是否存在
-func UserExist(c *gin.Context) {
-
-}
+// // 查询用户名是否存在
+// func UserExist(c *gin.Context) {
+// 
+// }
 
 // 添加用户
 func AddUser(c *gin.Context) {
-
+	var data model.User
+	if err := c.ShouldBindJSON(&data); err != nil {
+		// 返回错误信息
+		// gin.H封装了生成json数据的工具
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})	// 400
+		return
+	}
+	// 添加用户前先判断用户名是否存在
+	code := model.CheckUser(data.Username)
+	if code == errmsg.SUCCESS {
+		model.CreateUser(&data)
+	}
+	
+	c.JSON(http.StatusOK, gin.H{
+		"status" : code,
+		"data" : data,
+		"message" : errmsg.GetErrMsg(code)
+	})
 }
 
 // 查询单个用户
--- model/User.go
+++ model/User.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"blog/utils/errmsg"
+
 	"github.com/jinzhu/gorm"
 )
 
@@ -10,4 +12,24 @@ type User struct {
 	Username	string 		`gorm:"type:varchar(20);not null" json:"username"`
 	PassWord	string 		`gorm:"type:varchar(20);not null" json:"password"`
 	Role 		int 		`gorm:"type:int" json:"role"`
+}
+
+
+// 查询用户是否存在
+func CheckUser(name string) int {
+	var userList User
+	db.Select("id").Where("username = ?", name).First(&userList)
+	if userList.ID > 0 {
+		return errmsg.ERROR_USERNAME_USED	// 1001
+	}
+	return errmsg.SUCCESS		// 200
+}
+
+// 添加用户
+func CreateUser(data *User) int {
+	err := db.Create(&data).Error
+	if err != nil {
+		return errmsg.ERROR		// 500
+	}
+	return errmsg.SUCCESS		// 200
 }
